cc22c969cb849f166af5269fcddaa821c3a95d8d
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-04-08 12:57:45 +0545

    Home page added

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), 
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
