eab2e4aba699847f70faebb9b24bb05adfc5d210
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-04-10 12:51:45 +0545

    Home page modified

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from blogs.models import Blog, Category
 
 def home(request):
-    featured_posts = Blog.objects.filter(featured_post=True, status=1).order_by('-created_at')
+    featured_posts = Blog.objects.filter(featured_post=True, status=1).order_by('-created_at')[:1]
+    breaking_news = Blog.objects.filter(status=1, featured_post=False).order_by('-created_at')[:4]
     posts = Blog.objects.filter(status=1, featured_post=False).order_by('-created_at')
     context = {
         'featured_posts': featured_posts,
+        'breaking_news': breaking_news,
         'posts': posts
     }
     return render(request, 'home.html', context)
