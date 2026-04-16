6d8c57643f25e6b02668c663d71f3db0ed98bb62
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-04-16 13:04:07 +0545

    Post by category added

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Blog, Category
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def post_by_category(request, category_id):
+    posts = Blog.objects.filter(category=category_id, status=1)
+    category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'posts': posts,
+        'category': category
+    }
+    return render(request, 'posts_by_category.html', context)
